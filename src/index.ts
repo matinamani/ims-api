debd384bbe3eee3b7edf5eb5be42b404342dfdc7
Matin Amani <mtin.amani@gmail.com>
2024-11-17 18:07:40 +0330

    build: install express and @types/express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-const main = () => 'Hello World'
+import Express from 'express'
 
-console.log(main())
+const app = Express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!')
+})
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+)
